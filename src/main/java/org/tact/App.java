61fcaeb770b3f76ae2e70808d39724df5f2bbe09
Parthiban <msparthiruth@gmail.com>
2018-03-09 10:09:33 +0530

    javacode

--- src/main/java/org/tact/App.java
+++ src/main/java/org/tact/App.java
@@ -1,18 +1,26 @@
 package org.tact;
 
 import java.util.ArrayList;
-
-/**
- * Hello world!
- *
- */
+class a{
+	void show(){
+	System.out.println("class object a");
+	String s = "1b2n3m4j";
+	String b = " ";
+	for(int i=0;i< s.length()-1;i++){
+		if(s.charAt(i)>=48 && s.charAt(i)<= 57){
+			b += s.charAt(i);
+		}
+	}
+	System.out.println(b);
+	}
+}
 public class App 
 {
     public static void main( String[] args )
     {
-    	//get
-        ArrayList <String> al=new ArrayList<String>();
-        //al.add()
-         
+    	a s; // aggrecation
+    	 s= new a();
+    	s.show(); //agrrecation
+    	
     }
 }
