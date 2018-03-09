35876d743443fd9d58ba7c9310cf00c27ac2f47e
Parthiban <msparthiruth@gmail.com>
2018-03-09 10:36:55 +0530

    java code

---
+++ src/main/java/org/tact/MathTest.java
@@ -0,0 +1,23 @@
+package org.tact;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		
+	   int a = square(5);
+	   
+	   System.out.println(a);
+		
+	}
+	
+	public static int square(int a){
+
+		//int c = 4;
+		
+		//a = 5;
+		 
+		a *= a;
+		
+		return a;
+	}
+}
