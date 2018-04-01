4387bb7a655fbc84bc4489f2407c180e97a9433d
Parthiban <msparthiruth@gmail.com>
2018-04-01 16:13:11 +0530

    db values are grouped by topic

--- src/main/java/org/tact/BehavioralTest.java
+++ src/main/java/org/tact/BehavioralTest.java
@@ -2,7 +2,9 @@ package org.tact;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
@@ -10,37 +12,99 @@ import java.util.Set;
 public class BehavioralTest {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		//Scanner sc = new Scanner(System.in);
 		
-		ArrayList<String> javalist= new ArrayList<String>();
-		javalist.add("java");
-		javalist.add("java1");
+		List<Map<String, Object>> dbList= new ArrayList<Map<String, Object>>();
 		
-		ArrayList<String> nodejslist = new ArrayList<String>();
-		nodejslist.add("nodejs");
-		nodejslist.add("nodejs1");
-	
-		HashMap<String,ArrayList> hmt = new HashMap<String,ArrayList>();
-		hmt.put("java", javalist);
-		hmt.put("nodejs", nodejslist);
+		Map<String, Object> mp1 = new  HashMap<String, Object>();
+		mp1.put("question", "what is java uses");
+		mp1.put("code", "dummycode:1");
+		//mp1.put("level", 1);
+		//mp1.put("category", "medium");
+		mp1.put("topic", "java");
+		dbList.add(mp1);
+				
+		Map<String, Object> mp2 = new  HashMap<String, Object>();
+		mp2.put("question", "java q 1");
+		mp2.put("code", "dummycode:1");
+		//mp1.put("level", 1);
+		//mp1.put("category", "medium");
+		mp2.put("topic", "java");
+		dbList.add(mp2);
 		
-		/*System.out.println("Enter the topic:");
-		String topic = sc.next();
-		System.out.println(hmt.get(topic));*/
+		Map<String, Object> mp3 = new  HashMap<String, Object>();
+		mp3.put("question", "node q 1");
+		mp3.put("code", "dummycode:2");
+		//mp1.put("level", 1);
+		//mp1.put("category", "medium");
+		mp3.put("topic", "node");
+		dbList.add(mp3);
 		
-		Iterator it = hmt.entrySet().iterator() ;
-		while(it.hasNext()){
-			Map.Entry pair = (Map.Entry)it.next();
-			System.out.println(pair.getKey()+":");
-			System.out.println("   "+ pair.getValue());
-		}
+		Map<String, Object> mp4 = new  HashMap<String, Object>();
+		mp4.put("question", "node q 1");
+		mp4.put("code", "dummycode:3");
+		//mp1.put("level", 1);
+		//mp1.put("category", "medium");
+		mp4.put("topic", "node");
+		dbList.add(mp4);
+//		int numb=sc.nextInt();
+			HashSet<Object> s =new HashSet<Object>();
+			
+			for(Map<String,Object> entry : dbList){
+			    s.add(entry.get("topic"));
+			  //  System.out.println("d");
+			}
+			Iterator i=s.iterator();
+		       for(Object val:s){
+			    System.out.println("\""+val+"\" = [");
+			    String input=String.valueOf(val);
+			    for (Map<String, Object > entry : dbList) {
+			   	  if(entry.get("topic").equals(input)){
+			   	    Set entryset=entry.entrySet();
+			   	    //Iterator it=entryset.iterator();
+			   	   //System.out.println(entryset);
+			   	    System.out.print("    "+"{");
+			   	    int flag=0;
+			   	    for (Map.Entry<String,Object> e : entry.entrySet()) {
+			   	        if(flag==1){
+			   	            System.out.print(",");
+			   	        }
+			   	        flag=1;
+	                    System.out.print( e.getKey()+" = " + e.getValue());
+	              }
+	              System.out.print("}");
+			   	      System.out.println();
+			   	  }
+			}
+			System.out.println("]");
+			}
+			
+			
+			/*
+			 * "java" = [
+			 * 		{code=dummycode:1, question=what is java uses, topic=java},
+			 * 		{code=dummycode:1, question=j2, topic=java}
+			 * ],
+			 * 
+			 * "node" = [
+			 * 		{code=dummycode:1, question=node 1, topic=java},
+			 * 		{code=dummycode:1, question=node 2, topic=java}
+			 * ], 	
+			 */
+			
 			
 					
-	
+		}		
+		}
+
 		
-	}	
+		
+		
+	
+
+		
 	
 
 	
 
-}
+
