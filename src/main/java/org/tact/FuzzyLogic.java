a7e670fb4648cbc1272e3b4401e6cca5a5fc60af
Parthiban <msparthiruth@gmail.com>
2018-04-27 14:48:19 +0530

    TACT-911:add days and hours

---
+++ src/main/java/org/tact/FuzzyLogic.java
@@ -0,0 +1,36 @@
+package org.tact;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+//import org.ocpsoft.prettytime.PrettyTime;
+
+public class FuzzyLogic {
+
+	public static void main(String[] args) {
+		
+		//PrettyTime p = new PrettyTime(new Locale("fr"));
+		//System.out.println(p.format(new Date()));
+		try {
+	        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+	        Date past = format.parse("26/4/2018");
+	        Date now = new Date();
+            System.out.println("input Date:"+past);
+            System.out.println("TDY Date:"+now);
+	       // System.out.println(TimeUnit.MILLISECONDS.toMillis(now.getTime() - past.getTime()) + " milliseconds ago");
+	       // System.out.println(TimeUnit.MILLISECONDS.toMinutes(now.getTime() - past.getTime()) + " minutes ago");
+            if(TimeUnit.MILLISECONDS.toHours(now.getTime() - past.getTime())<24){
+            	 System.out.println(TimeUnit.MILLISECONDS.toHours(now.getTime() - past.getTime()) + " hours ago");
+            }else{
+	        //System.out.println(TimeUnit.MILLISECONDS.toHours(now.getTime() - past.getTime()) + " hours ago");
+	        System.out.println(TimeUnit.MILLISECONDS.toDays(now.getTime() - past.getTime()) + " days ago");
+            }
+           // System.out.println(TimeUnit.MILLISECONDS.toWeeks(now.getTime() - past.getTime()) + " days ago");
+	    }
+	    catch (Exception j){
+	        j.printStackTrace();
+	    }
+	}
+
+}
